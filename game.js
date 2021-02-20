d47b0f09793d0e53167af82fc4450788c889a7b5
Rodrigo <rmoraisu@protonmail.com>
2021-02-20 17:06:24 -0300

    :basecampy: Added basics for pixijs
    Useful refence links for the future:
    JS Array.concat()
    https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
    https://medium.com/swlh/inside-pixijss-high-performance-update-loop-856fb1d841a0
    https://pixijs.io/examples/#/graphics/dynamic.js

---
+++ game.js
@@ -0,0 +1,14 @@
+/*
+IDEAS:
+- user interface for level generation 
+- endless mode
+*/
+
+// NOTES:
+// non-specified dimentions: 160x100 sample images (1.6 ratio)
+
+let growy = new Growy()
+
+growy.gameLoop()
+
+
